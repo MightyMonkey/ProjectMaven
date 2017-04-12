ff40c0942dee67eeba37b3eebd06a12da0395aec
Chastain Camille <cchastai@SA203-07A.ensi-bourges.fr>
2017-04-12 16:20:13 +0200

    ajout de la v2

--- jf/src/main/java/jf/App.java
+++ jf/src/main/java/jf/App.java
@@ -1,5 +1,11 @@
 package jf;
 
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 /**
  * Hello world!
  *
@@ -12,9 +18,37 @@ public class App
         System.out.println( "Hello World!" );
 	int a= instance.max(4,5);
 	System.out.println("max: "+a);
+
+	int monmax=0;
+	int i,j,nb=0;
+
+	CSVReader reader;
+	try {
+		reader = new CSVReader(new FileReader("data.csv"),',');
+	
+
+	 	List<String[]> myEntries= reader.readAll();
+
+		for(i=0;i<myEntries.size();i++){
+			for(j=0;j<myEntries.get(i).length;j++){
+				nb=Integer.parseInt(myEntries.get(i)[j]);
+				System.out.println("Nombre lu: "+nb);
+				monmax=instance.max(monmax,nb);
+			}
+		}
+		System.out.println("Nombre max lu: "+monmax);
+		
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
     }
     public int max(int a,int b)
 	{
-		return a;//le stagiaire est passé par là  >b ? a:b;
+		return a>b ? a:b;
 	}
 }
